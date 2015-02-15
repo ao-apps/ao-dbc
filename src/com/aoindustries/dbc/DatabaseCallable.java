c6a6b4be509ec78c4f85bef1fd3c133b14de5a2e
Dan Armstrong <dan@aoindustries.com>
2015-02-15 17:35:28 +0000

    Moving database abstraction from aocode-public into new ao-dbc project

---
+++ src/com/aoindustries/dbc/DatabaseCallable.java
@@ -0,0 +1,38 @@
+/*
+ * ao-dbc - Simplified JDBC access for simplified code.
+ * Copyright (C) 2010, 2011, 2014, 2015  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-dbc.
+ *
+ * ao-dbc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-dbc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-dbc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.dbc;
+
+import java.sql.SQLException;
+
+/**
+ * Target that may be used by <code>Database.executeTransaction</code>.
+ *
+ * @see  Database#executeTransaction(com.aoindustries.sql.DatabaseCallable)
+ *
+ * @author  AO Industries, Inc.
+ */
+public interface DatabaseCallable<V> extends DatabaseCallableE<V,RuntimeException> {
+
+	@Override
+    V call(DatabaseConnection db) throws SQLException;
+}
---
+++ src/com/aoindustries/dbc/ObjectFactoryE.java
@@ -0,0 +1,39 @@
+/*
+ * ao-dbc - Simplified JDBC access for simplified code.
+ * Copyright (C) 2014, 2015  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-dbc.
+ *
+ * ao-dbc is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-dbc is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-dbc.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.aoindustries.dbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Creates instances of objects of the generics type from a result set.
+ *
+ * @author  AO Industries, Inc.
+ */
+public interface ObjectFactoryE<T,E extends Exception> {
+
+    /**
+     * Creates one object from the current values in the ResultSet.
+     */
+    T createObject(ResultSet result) throws SQLException, E;
+}
