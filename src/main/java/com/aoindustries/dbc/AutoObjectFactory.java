2955f7dd8f4261dd763a853a3626f2a032477600
AO Industries, Inc <support@aoindustries.com>
2021-01-02 05:43:35 -0600

    TODO: Recognize valueOf with Optional return?

--- src/main/java/com/aoindustries/dbc/AutoObjectFactory.java
+++ src/main/java/com/aoindustries/dbc/AutoObjectFactory.java
@@ -1,6 +1,6 @@
 /*
  * ao-dbc - Simplified JDBC access for simplified code.
- * Copyright (C) 2009, 2010, 2011, 2013, 2015, 2016, 2020  AO Industries, Inc.
+ * Copyright (C) 2009, 2010, 2011, 2013, 2015, 2016, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -44,6 +44,7 @@ import java.util.concurrent.ConcurrentMap;
  *
  * @author  AO Industries, Inc.
  */
+// TODO: Recognize valueOf with Optional return?  Generic type erasure could be a pain.
 public class AutoObjectFactory<T> implements ObjectFactory<T> {
 
 	static final String EOL = System.lineSeparator();
